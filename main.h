cb3743b9cec9a04adb359ee469b9fbce6c154bb8
ThatsVie <Viepaulalmt@gmail.com>
2023-11-03 13:32:25 -0700

    corrected typos in main.h

--- main.h
+++ main.h
@@ -7,5 +7,5 @@ void format_string(int *char_count, va_list args);
 void format_int(int *char_count, va_list args);
 void format_percent(int *char_count);
 void handle_format(int *char_count, const char **format, va_list args);
-void print_int(int *char_countm int num);
+void print_int(int *char_count int num);
 #endif
