2bde2e77d225007c3f835cf03386f25e65c1d197
ThatsVie <Viepaulalmt@gmail.com>
2023-11-03 16:24:16 -0700

    removing white space

--- _printf.c
+++ _printf.c
@@ -6,27 +6,27 @@ int _printf(const char *format, ...)
 {
 	int char_count = 0;
 	va_list args;
-
+	
 	if (format == NULL)
 	{
 		return (-1);
 	}
-
-        va_start(args, format);
+	
+	va_start(args, format);
 	
 	while(*format)
-        {
+	{
 		if (*format !='%')
-                {
+		{
 			putchar(*format);
 			char_count++;
 		}
 		else
 		{
-			handle_format(&char_count, &format, args);
-                }
+			handle_format(&char_count, &format, args)
+		}
 		format++;
-        }
-        va_end(args);
-       	return (char_count);
+	}
+	va_end(args);
+	return (char_count);
 }
