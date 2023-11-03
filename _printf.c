220be243de801364963944e6898d1176c8ea05f0
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-11-02 19:22:31 -0700

    trying to condense code

--- _printf.c
+++ _printf.c
@@ -6,105 +6,51 @@
  *
  * @format: format specifier
  *
- * Return:
+ * Return: counter
  */
-
 int _printf(const char *format, ...)
 {
 	int char_count = 0;
-	char ch;
-	const char *str;
-	int num;
-	int temp;
-	int digits;
-	int divisor;
-	int i;
-	int digit;
 	va_list args;
-
-	if (format == NULL)
-		return (-1);
-
 	va_start(args, format);
 
-	while (*format)
+	while (*format != '\0')
 	{
-		if (*format != '%')
-		{
-			putchar(*format);
-			char_count++;
-		}
-		else
+		if (*format == '%')
 		{
 			format++;
 
+			if (*format =='%')
+			{
+				putchar('%');
+				char_count++;
+			}
 			if (*format == 'c')
 			{	
-				ch = va_arg(args, int);
-				putchar(ch);
+				int c = va_arg(args, int);
+				putchar(c);
 				char_count++;
 			}
 			else if (*format == 's')
 			{
-				str = va_arg(args, const char *);
-				while (*str)
+				const char *s = va_arg(args, const char *);
+				while (*s != '\0')
 				{
-					putchar(*str);
+					putchar(*s);
+					s++;
 					char_count++;
-					str++;
 				}
-			}
-			else if (*format == 'd' || *format == 'i')
-			{
-				num = va_arg(args, int);
-
-				if (num < 0)
-				{
-					putchar('-');
-					char_count++;
-					num = -num;
-				}
-
-				temp = num;
-				digits = 0;
-				do
-				{
-				temp /= 10;
-				digits++;
-				}
-				while (temp);
-
-				temp = num;
-				divisor = 1;
-				for (i = digits - 1; i > 0; i--)
-				{
-					divisor *= 10;
-				}
-			
-				while (digits > 0)
-				{
-				digit = num / divisor;
-				num %= divisor;
-				putchar('0' + digit);
-				char_count++;
-				digits--;
-				divisor = 1;
-				}
-			}
-			else if (*format == '%')
-			{
-				putchar('%');
-				char_count++;
 			}
 			else
 			{
-				putchar('%');
 				putchar(*format);
-				char_count += 2;
+				char_count++;
 			}
 		}
+
 		format++;
 	}
 	va_end(args);
+
 	return (char_count);
 }
