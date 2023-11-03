87d54cb2b943d7087bda45a52ace95f2d82ccbc8
ThatsVie <Viepaulalmt@gmail.com>
2023-11-03 13:05:40 -0700

    added function prototypes to main.h

--- main.h
+++ main.h
@@ -2,5 +2,9 @@
 #define _HEADER_
 
 int _printf(const char *format, ...);
-
+void format_char(int *char_count, va_list args);
+void format_string(int *char_count, va_list args);
+void format_int(int *char_count, va_list args);
+void format_percent(int *char_count);
+void handle_format(int *char_count, const char **format, va_list args)
 #endif
