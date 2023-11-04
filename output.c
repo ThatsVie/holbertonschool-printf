4a03d1964b735c082a401bb0a9c565318aa81858
ThatsVie <Viepaulalmt@gmail.com>
2023-11-04 15:42:16 -0700

    trying something different for print int

--- output.c
+++ output.c
@@ -3,46 +3,17 @@
 /**
  * print_int - formats and prints and integer
  *
- * @char_count: a pointer to an integer that
- * keeps track of the number of characters printed
- * @num: the integer value to be formatted and printed
+ * @values: va_list containing integer to be printed
+ *
+ * Return: number of characters written
  */
-void print_int(int *char_count, int num)
+int print_int (va_list values)
 {
-	char int_str[12];
-	int i = 0;
-	int j;
-	char reversed;
+	int num = va_arg(values, int);
+	int count = 0;
+	char buffer[12];
 
-	if (num < 0)
-	{
-		int_str[i++] = '_';
-		num = -num;
-	}
-
-	if (num == 0)
-	{
-		int_str[i++] = '0';
-	}
-	else
-	{
-		while (num > 0)
-		{
-			int_str[i++] = (char)('0' + (num % 10));
-			num /= 10;
-		}
-	}
-
-	reversed[12];
-	for (j = 0; i > 0; j++, i--)
-	{
-		reversed[j] = int_str[i - 1];
-	}
-	reversed[j] = '\0';
-
-	for (i = 0; reversed[i] != '\0'; i++)
-	{
-		putchar(reversed[i]);
-		(*char_count)++;
-	}
+	count += sprintf(buffer, "%d", num);
+	write(1, buffer, count);
+	return (count);
 }
