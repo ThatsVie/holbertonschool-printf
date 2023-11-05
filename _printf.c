1d28b1bd2aef06edc6d6b0f0f4c22a880bad048b
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-11-05 10:47:42 -0800

    working on changing how extra = is handled

--- _printf.c
+++ _printf.c
@@ -33,21 +33,56 @@ int _printf(const char *format, ...)
 			count += _putchar(format[a]);
 		else
 		{
-			for (b = 0; b < 5; b++)
+			if (format[a + 1] == 'd' || format[a + 1] == 'i')
+				count += handleIntegerSpecifier(&args);
+			else
 			{
-				if (*specifiers[b].specifier == format[a + 1])
+				for (b = 0; b < 5; b++)
 				{
-					format++;
-					count += specifiers[b].function(args);
-					break;
+					if (*specifiers[b].specifier == format[a + 1])
+					{
+						format++;
+						count += specifiers[b].function(args);
+						break;
+					}
 				}
+				if (b == 5 && format[a + 1] == '\0')
+					return (-1);
+				else if (b == 5 && format[a + 1] != '\0')
+					count += _putchar(format[a]);
 			}
-			if (b == 5 && format[a + 1] == '\0')
-				return (-1);
-			else if (b == 5 && format[a + 1] != '\0')
-				count += _putchar(format[a]);
 		}
 	}
 	va_end(args);
 	return (count);
 }
+
+int handleIntegerSpecifier(va_list *args)
+{
+	int value = va_arg(*args, int);
+	int isneg = 0;
+	int numdig = 0;
+	int temp;
+
+	if (value < 0)
+	{
+		isneg = 1;
+		value = -value;
+	}
+	if (value == 0)
+		return _putchar('\0');
+
+	temp = value;
+
+	while (temp != 0)
+	{
+		temp /= 10;
+		numdig++;
+	}
+
+	char buffer[numdig + isneg];
+
+	if (isneg)
+		buffer[] = '-';
+
+
