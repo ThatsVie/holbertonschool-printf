7c7b9ecf72645738d378d68b4ceb4664e9c5248f
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-11-04 19:52:32 -0700

    correcting test and checking counter

--- format_handlers.c
+++ format_handlers.c
@@ -80,7 +80,6 @@ void handle_format(int *char_count, const char **format, va_list args)
 	{
 		putchar('%');
 		(*char_count)++;
-		(*format)++;
 	}
 	else if (**format == 'c')
 		format_char(char_count, args);
@@ -92,6 +91,6 @@ void handle_format(int *char_count, const char **format, va_list args)
 	{
 		putchar('%');
 		putchar(**format);
-		(*char_count) += 2;
+		(*char_count)++;
 	}
 }
