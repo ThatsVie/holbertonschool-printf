230fbd717e560715f5869169481c0af12bd74a62
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-11-06 09:46:33 -0800

    commenting on print_functions

--- print_functions.c
+++ print_functions.c
@@ -8,9 +8,9 @@
 
 int _printChar(va_list args)
 {
-	char c = va_arg(args, int);
+	char c = va_arg(args, int);/*Get the next argument of type int from va_list*/
 
-	return (write(1, &c, 1));
+	return (write(1, &c, 1));/*Write the character to standard output and return the number of characters written*/
 }
 
 /**
@@ -22,11 +22,11 @@ int _printChar(va_list args)
 
 int _printString(va_list args)
 {
-	char *s = va_arg(args, char *);
+	char *s = va_arg(args, char *);/*Get the next argument of type char* from va_list*/
 
 	if (s == NULL)
-		s = "(null)";
-	return (write(1, s, strlen(s)));
+		s = "(null)";/*If the string is NULL, assign a default value*/
+	return (write(1, s, strlen(s)));/*Write the string to standard output and return the number of characters written*/
 }
 
 /**
@@ -38,52 +38,52 @@ int _printString(va_list args)
 
 int _printInt(va_list args)
 {
-	char buffer[20];
-	long int num;
-	int isneg = 0;
+	char buffer[20];/*Buffer to store the integer as a string*/
+	long int num;/*Variable to store the integer value*/
+	int isneg = 0;/*Flag to indicate if the number is negative*/
 	int len, j;
 	int i = 0;
 
 	if (sizeof(int) == sizeof(long int))
 	{
-		num = va_arg(args, long int);
+		num = va_arg(args, long int);/*Get the next argument of type long int from va_list*/
 	}
 	else
 	{
-		num = va_arg(args, int);
+		num = va_arg(args, int);/*Get the next argument of type int from va_list*/
 	}
 	if (num < 0)
 	{
-		isneg = 1;
-		num = -num;
+		isneg = 1;/*Set the flag if the number is negative*/
+		num = -num;/*Convert the number to positive*/
 	}
 	do {
-		buffer[i++] = num % 10 + '0';
+		buffer[i++] = num % 10 + '0';/*Convert each digit to a character and store it in the buffer*/
 		num /= 10;
 	}
 
 	while (num > 0);
-	len = strlen(buffer);
+	len = strlen(buffer);/*Calculate the length of the number*/
 	if (isneg)
 	{
-		buffer[i++] = '-';
+		buffer[i++] = '-';/*Add a '-' sign if the number is negative*/
 	}
-	len = i;
+	len = i;/*Update the length with the actual number of characters*/
 	for (j = 0; j < i / 2; j++)
 
 	{
-		char temp = buffer[j];
+		char temp = buffer[j];/*Swap the characters in the buffer to reverse the number*/
 
 		buffer[j] = buffer[i - j - 1];
 		buffer[i - j - 1] = temp;
 
 	}
-	return (write(1, buffer, len));
+	return (write(1, buffer, len));/*Write the number to standard output and return the number of characters written*/
 }
 PrintFunction specifiers[] = {
 	{"d", _printInt},
 	{"i", _printInt},
-};
+};/*Register the print function for 'd' and 'i' format specifier*/
 
 /**
  * _printPercent - print  a percent sign
@@ -94,8 +94,8 @@ PrintFunction specifiers[] = {
 int _printPercent(va_list args)
 {
 	char p = '%';
-	(void)args;
-	return (write(1, &p, 1));
+	(void)args;/*Silence the unused argument warning*/
+	return (write(1, &p, 1));/*Write the percent sign to standard output and return the number of characters written*/
 }
 /**
  * _putchar - writes character to standard output
@@ -106,5 +106,5 @@ int _printPercent(va_list args)
 
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+	return (write(1, &c, 1));/*Write the character to standard output and return the number of characters written*/
 }
